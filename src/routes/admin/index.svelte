2e0ddc37db49cb547a4e8d9cebfa8b960900a097
Jochem van der Valk <jochemvandervalk@gmail.com>
2021-10-18 23:55:00 +0200

    Admin page
    netilfy-cms-app is loaded on mount, it gave a "window is not defined" error when imported normally

---
+++ src/routes/admin/index.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { onMount } from 'svelte';
+	import 'netlify-identity-widget';
+
+	onMount(async () => {
+		const CMS = await import('netlify-cms-app');
+		CMS.init();
+	});
+</script>
